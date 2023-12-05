709ee9ee806c6e56888f791122d22935afd82213
Anand Dhar Dwivedi <120220077+add0203@users.noreply.github.com>
2023-12-05 10:47:54 +0530

    contact.jsx

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -13,7 +13,31 @@ import { ThemeContext } from "../../context";
 
 const Contact = () => {
   <style>
-    .pushable {
+     {`
+          .pushable {
+            background: hsl(340deg 100% 32%);
+            border-radius: 12px;
+            border: none;
+            padding: 0;
+            cursor: pointer;
+            outline-offset: 4px;
+          }
+
+          .front {
+            display: block;
+            padding: 12px 42px;
+            border-radius: 12px;
+            font-size: 1.25rem;
+            background: hsl(345deg 100% 47%);
+            color: white;
+            transform: translateY(-6px);
+          }
+
+          .pushable:active .front {
+            transform: translateY(-2px);
+          }
+        `}
+{/*     .pushable {
     background: hsl(340deg 100% 32%);
     border-radius: 12px;
     border: none;
@@ -33,7 +57,7 @@ const Contact = () => {
 
   .pushable:active .front {
     transform: translateY(-2px);
-              }
+              } */}
 </style>
   const formRef = useRef();
   const [done, setDone] = useState(false);
