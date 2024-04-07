2e0de6c0b21fffe7107c097e5f6078cd0db27f2b
Anand Dhar Dwivedi <120220077+add0203@users.noreply.github.com>
2024-04-07 19:11:58 +0530

    data.js

--- src/components/data.js
+++ src/components/data.js
@@ -1,3 +1,72 @@
+// import ui from "../img/ui.png";
+// import reactimg from "../img/react.png";
+// import nodeimg from "../img/node.png";
+// import ui2 from "../img/ui2.png";
+// import portfolio from "../img/portfolio.png";
+// // slider image
+
+// import img1 from "../img/carousel/1.jpg";
+// import img2 from "../img/carousel/2.jpg";
+// import img3 from "../img/carousel/3.jpg";
+// import img4 from "../img/carousel/4.jpg";
+// import img5 from "../img/carousel/5.jpg";
+// import img6 from "../img/carousel/6.jpg";
+// import img7 from "../img/carousel/7.jpg";
+// import img8 from "../img/carousel/8.jpg";
+// import img9 from "../img/carousel/9.jpg";
+
+// export const slides = [
+//   { url: img9 },
+//   { url: img1 },
+//   { url: img2 },
+//   { url: img3 },
+//   { url: img5 },
+//   { url: img6 },
+//   { url: img7 },
+//   { url: img8 },
+//   { url: img4 },
+// ];
+// export const projects = [
+//   {
+//     id: 1,
+//     link: "https://github.com/add0203/socialmedia_backend.git",
+//     img: nodeimg,
+//   },
+//   {
+//     id: 2,
+//     link: "",
+//     img: ui,
+//   },
+//   {
+//     id: 3,
+//     link: "https://github.com/add0203/movies_api.git",
+//     img: reactimg,
+//   },
+//   {
+//     id: 4,
+//     link: "",
+//     img: ui2,
+//   },
+//   {
+//     id: 5,
+//     link: "https://anand-dhar-dwivedi.vercel.app/",
+//     img: portfolio,
+//   },
+// ];
+// export const RoughData = [
+//   {
+//     id: 1,
+//     mess: "its not ok yet",
+//   },
+//   {
+//     id: 2,
+//     mess: "its not ok",
+//   },
+// ];
+
+
+
+
 import ui from "../img/ui.png";
 import reactimg from "../img/react.png";
 import nodeimg from "../img/node.png";
@@ -29,26 +98,37 @@ export const slides = [
 export const projects = [
   {
     id: 1,
-    link: "https://github.com/add0203/socialmedia_backend.git",
+    name: "Web Chat Application (Team)",
+    link: "https://github.com/add0203/web_chatapp.git",
     img: nodeimg,
   },
   {
     id: 2,
-    link: "",
+    name: "API on Authorization ",
+    link: "https://github.com/add0203/User-authentication-api",
     img: ui,
   },
   {
     id: 3,
+    name: "API on Authorization ",
     link: "https://github.com/add0203/movies_api.git",
     img: reactimg,
   },
   {
     id: 4,
-    link: "",
+    name: "backend of the eshop",
+    link: "https://github.com/add0203/node-js-backend-.git",
     img: ui2,
   },
   {
     id: 5,
+    name: "Portfolio",
+    link: "https://anand-dhar-dwivedi.vercel.app/",
+    img: portfolio,
+  },
+  {
+    id: 6,
+    name: "Portfolio 2",
     link: "https://anand-dhar-dwivedi.vercel.app/",
     img: portfolio,
   },
